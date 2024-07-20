128d37c9edd0f9e050c38a8a42780dee92b6754b
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-07-20 11:48:11 +0530

    ok

--- DAY-3/findIndex of1stOccin==astr.cpp
+++ DAY-3/findIndex of1stOccin==astr.cpp
@@ -5,19 +5,11 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        int j=0;
-        for(int i=0 ; i<n ; i++){
-            if(needle[j]==haystack[i]){
-                j++;
-            }
-            //very imp step for updation
-            else{
-                i=i-j;
-                j=0;
-            }
-            if(j==m) return i-j+1;
+        int n = haystack.length();
+        int m = needle.length();
+        int j = 0;
+        for (int i = 0; i <=n-m; i++) {
+             if(haystack.substr(i,m)==needle) return i;
         }
         return -1;
     }
--- DAY-3/integerToRoman.cpp
+++ DAY-3/integerToRoman.cpp
@@ -4,11 +4,20 @@ using namespace std;
 class Solution {
 public:
     string intToRoman(int num) {
-        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        //kind of mapping with  indices by setting 2 vector arrays of enough to get the ans
+        static vector <int> val={1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1};
+        static vector <string> sym={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string ans="";
+
+        for(int i=0 ; i<val.size() ; i++){
+            if(num==0) break;
+
+            int times=num/val[i];
+            while(times--){
+                ans+=sym[i];
+            }
+            num%=val[i]; //so to continuosly get the roman symbol of next digit of a number
+        }
+        return ans;
     }
 };
